be311d28c797e7d34b102ce939e9b65b606a562f
Parisj7 <parisjjfranco@gmail.com>
2022-04-20 21:13:55 -0300

    repush

--- catalog-service-parcial/src/main/java/com/dh/catalogservice/api/service/impl/CatalogServiceImpl.java
+++ catalog-service-parcial/src/main/java/com/dh/catalogservice/api/service/impl/CatalogServiceImpl.java
@@ -6,9 +6,13 @@ import com.dh.catalogservice.domain.model.MovieWS;
 import com.dh.catalogservice.domain.model.SeriesWS;
 import com.dh.catalogservice.domain.repository.MovieRepository;
 import com.dh.catalogservice.domain.repository.SeriesRepository;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,11 +24,17 @@ public class CatalogServiceImpl implements CatalogService {
     @Autowired
     private SeriesRepository serieRepository;
 
+    @CircuitBreaker(name = "catalog", fallbackMethod = "findAllEmptyFallback")
+    @Retry(name = "catalog")
     @Override
     public CatalogWS getCatalogByGenre(String genre){
         List<MovieWS> moviesWS = getMoviesByGenre(genre);
         List<SeriesWS> seriesWS = getSeriesByGenre(genre);
         return new CatalogWS(genre,moviesWS,seriesWS);
+    }
+
+    private CatalogWS findAllEmptyFallback(CallNotPermittedException exception) {
+        return new CatalogWS();
     }
 
 
